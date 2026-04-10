5ccd1df5146bb00dfda663a56b68e4859f08ab57
EnzoGuerato <guerato.enzo@gmail.com>
2026-04-10 10:32:04 -0300

    Criação Model User e Comentario

---
+++ src/main/java/br/com/fluxocaixa/projetotcc/model/Comentario.java
@@ -0,0 +1,47 @@
+package br.com.fluxocaixa.projetotcc.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+import lombok.EqualsAndHashCode;
+
+import java.util.Date;
+
+@Entity
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Table(name = "comentario")
+public class Comentario {
+
+    @Id
+    @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // fazer dps
+    // private Post post;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "parent_id")
+    private Comentario comentario;
+
+    @NotBlank
+    @Column(name = "conteudo_texto")
+    private String conteudoTexto;
+
+    // fazer dps
+    // private Midia midia
+
+
+    @PositiveOrZero
+    private Long upvotes;
+
+    @Column(name = "data_publicacao")
+    private Date dataPublicacao;
+}
---
+++ src/main/java/br/com/fluxocaixa/projetotcc/model/User.java
@@ -0,0 +1,38 @@
+package br.com.fluxocaixa.projetotcc.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PositiveOrZero;
+import lombok.EqualsAndHashCode;
+
+import java.util.Date;
+
+@Entity
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Table(name = "user")
+public class User {
+    @Id
+    @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String user;
+
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    private String senha;
+
+    @PositiveOrZero
+    @Column(name = "xp_total")
+    private Long xpTotal;
+
+    @DecimalMin("1")
+    private Long nivel;
+
+    @Column(name = "data_criacao")
+    private Date dataCriacao;
+}
